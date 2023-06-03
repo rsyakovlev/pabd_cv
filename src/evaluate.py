e5ae9d8b9a96d5a2525cac629b2170be97aaae4a
221781 <venom1994@inbox.ru>
2023-06-03 13:12:25 +0300

    evaluate.py modified

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,23 +1,33 @@
 import tensorflow as tf
 import os
 
-model = tf.keras.models.load_model('models/dummy_model')
+model = tf.keras.models.load_model('C:/Users/221781/PycharmProjects/pabd_cv/models/my_model')
 
 def classify_binary(img_path: str) -> int:
     img = tf.keras.utils.load_img(img_path, target_size=(180, 180))
-    # img = tf.io.decode_jpeg(data)
     img_t = tf.expand_dims(img, axis=0)
-
     predictions = model.predict(img_t)
     dog_probability = float(predictions[0])
     print(dog_probability)
     idx = dog_probability > 0.5
     return int(idx)
 
+def resize(img, label):
+    img_r = tf.image.resize(img, (180, 180))
+    return img_r, label
+
 if __name__ == '__main__':
-    cat_imgs = os.listdir('data/raw/pinterest/Cat')
-    y_pred = [
-        classify_binary(
-            os.path.join('data/raw/pinterest/Cat', img_path)) for img_path in cat_imgs
+    data_dir = 'C:/Users/221781/PycharmProjects/pabd_cv/data/raw/pinterest'
+    test_dataset = tf.keras.utils.image_dataset_from_directory(data_dir)
+    test_dataset = test_dataset.map(resize)
+    metrics = [
+        tf.keras.metrics.BinaryAccuracy(),
+        tf.keras.metrics.Recall(),
+        tf.keras.metrics.Precision()
     ]
-    print(y_pred)
+    model.compile(metrics=metrics)
+    history = model.evaluate(test_dataset)
+    loss, accuracy, precision, recall = history
+    print(f'Accuracy: {accuracy}')
+    print(f'Precision: {accuracy}')
+    print(f'Recall: {accuracy}')
